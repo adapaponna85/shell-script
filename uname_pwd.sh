d2afad924ee157d2b657b257bd8714b70bf23800
adapaponna85 <adapaponna85@gmail.com>
2023-12-15 05:38:43 -0500

    added commit

--- uname_pwd.sh
+++ uname_pwd.sh
@@ -7,10 +7,10 @@ echo "Enter the user name"
 
 read -s uname
 
-echo "user name entered is"
+echo "user name entered is: $uname"
 
 echo "Enter the password"
 
 read -s pwd
 
-echo "password entered is"
+echo "password entered is: $read"
