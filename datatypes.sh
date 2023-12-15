fd8f2a8963e2e8b5be18322ac8261cccb7995a5b
adapaponna85 <adapaponna85@gmail.com>
2023-12-15 05:49:23 -0500

    added commit

--- datatypes.sh
+++ datatypes.sh
@@ -3,9 +3,9 @@
 Num1=$1
 Num2=$2
 
-SUM=$(($Num1+$Num2))
+Sum=$(($Num1+$Num2))
 
-echo "Total is : $SUM"
+echo "Total is : $Sum"
 
 echo "How many arguments passed is: $#"
 echo "All arguments passed is : $@"
