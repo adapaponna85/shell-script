1f84d2037843ef6a01e491f32c3d1abc6681b476
adapaponna85 <adapaponna85@gmail.com>
2023-12-15 05:58:08 -0500

    added commit

--- datatypes.sh
+++ datatypes.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-
 NUMBER1=$1
 NUMBER2=$2
 
@@ -13,14 +12,3 @@ echo "How many args passed:: $#"
 echo "All args passed:: $@"
 
 echo "Script name:: $0"
-
-Num1=$1
-Num2=$2
-
-Sum=$(($Num1+$Num2))
-
-echo "Total is : $Sum"
-
-echo "How many arguments passed is: $#"
-echo "All arguments passed is : $@"
-echo file name is : $0"
