8686abbb992354ca926b34c5c3f92843d51f5c5f
adapaponna85 <adapaponna85@gmail.com>
2023-12-15 05:51:35 -0500

    added commit

--- datatypes.sh
+++ datatypes.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
 
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "Total:: $SUM"
+
+echo "How many args passed:: $#"
+
+echo "All args passed:: $@"
+
+echo "Script name:: $0"
+
 Num1=$1
 Num2=$2
 
